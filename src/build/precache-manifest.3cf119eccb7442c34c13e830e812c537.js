f2079a81c90aa16d7358626ea5fb226fa3696b40
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-04-14 10:14:34 +0400

    First commit

---
+++ src/build/precache-manifest.3cf119eccb7442c34c13e830e812c537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888113dca0f9f3f579d3b47acca6a252",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e75dbde9b1e92cacac3e",
+    "url": "/static/css/main.30ef5390.chunk.css"
+  },
+  {
+    "revision": "83fe54a3d45dcd0de073",
+    "url": "/static/js/2.8da14d85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8da14d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e75dbde9b1e92cacac3e",
+    "url": "/static/js/main.649c2503.chunk.js"
+  },
+  {
+    "revision": "430711db2cc41196af7a",
+    "url": "/static/js/runtime-main.9798f641.js"
+  }
+]);
